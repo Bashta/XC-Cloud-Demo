43c6133af0360433b31fed7ca0c59a9a874d311a
Erison Veshi <erison.veshi@ingka.ikea.com>
2022-06-15 09:51:41 +0200

    Initial Commit

---
+++ XCodeCoudDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XCodeCoudDemo
+//
+//  Created by Erison Veshi on 15/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XCodeCoudDemo/XCodeCoudDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeCoudDemoApp.swift
+//  XCodeCoudDemo
+//
+//  Created by Erison Veshi on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct XCodeCoudDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
